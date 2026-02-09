991e246f202efeef0f1463aecb5bcf24b79973b9
Snu Seam <siamahme766@gmail.com>
2026-02-09 13:12:10 +0600

    commit

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ const initDB = async () =>{
     await pool.query(`CREATE TABLE IF NOT EXISTS todos(
         id SERIAL PRIMARY  KEY,
         user_id INT REFERENCES users(id) ON DELETE CASCADE,
-        tittle VARCHAR(200) NOT NULL,
+        title VARCHAR(200) NOT NULL,
         description TEXT,
         completed BOOLEAN DEFAULT false,
         due_date DATE,
@@ -44,6 +44,8 @@ app.get('/', (req:Request, res:Response) => {
   res.send('Hello next level developers!')
 })
 
+
+// users crud
 app.post('/users', async(req:Request,res :Response)=>{
     const {name,email} = req.body;
 
@@ -152,6 +154,99 @@ app.delete('/users/:id', async (req:Request, res:Response)=>{
         res.status(500).json({
             success : false,
             message : err.message
+        })
+    }
+})
+
+// todos crud
+
+app.post('/todos', async (req: Request,res:Response)=>{
+
+    const {user_id,title} = req.body;
+    
+
+    try{
+        const result = await pool.query(`INSERT INTO todos(user_id,title) VALUES($1,$2) 
+            RETURNING *`,[user_id,title])
+
+            console.log(result);
+        res.status(201).json({
+            success: true,
+            message: "Todos Created",
+            data : result.rows[0]
+        })
+    }catch(err : any){
+        res.status(500).json({
+            success : false,
+            message : err.message
+        })
+    }
+}
+)
+
+app.get('/todos', async(req:Request,res:Response)=>{
+    try{
+        const result = await pool.query(`SELECT * FROM todos`)
+
+        res.status(200).json({
+            success: true,
+            message: "All data get",
+            data : result.rows
+        })
+    }catch(err : any){
+        res.status(500).json({
+            success: false,
+            message : err.message
+        })
+    }
+})
+
+app.get('/todos/:id', async (req:Request,res:Response)=>{
+    try{
+        const result = await pool.query(`SELECT * FROM todos WHERE id = $1`, [req.params.id])
+
+        if(result.rows.length === 0 ){
+            res.status(404).json({
+                success: false,
+                message : "Not found"
+            })
+        }else{
+            res.status(200).json({
+                success: true,
+                message : "data found",
+                data : result.rows[0]
+            })
+        }
+    }catch (err : any) {
+        res.status(500).json({
+            success: false,
+            message : "data not found"
+        })
+    }
+})
+
+app.put('/todos/:id', async(req:Request, res: Response)=>{
+    const {user_id,title} = req.body;
+
+    try{
+        const result = await pool.query(`UPDATE todos SET user_id=$1 , title =$2 WHERE id = $3 RETURNING *`, [user_id,title,req.params.id])
+
+        if(result.rows.length === 0){
+            res.status(404).json({
+                success : false,
+                message : " not found"
+            }
+        )
+        }else{
+            res.status(200).json({
+                success: true,
+                data : result.rows[0]
+            })
+        }
+    }catch(err: any) {
+        res.status(500).json({
+            success: false,
+            message : "Data not updated"
         })
     }
 })
