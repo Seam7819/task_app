5cd06b4ccbf3ac419684406920719e2c3c561031
Snu Seam <siamahme766@gmail.com>
2026-02-10 07:31:33 +0600

    d

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,20 @@
-import express, { Request, Response } from "express"
-import {Pool} from "pg"
-import dotenv from "dotenv"
-import path from "path"
+import express, { NextFunction, Request, Response } from "express"
+import config from "./config"
+import initDB, { pool } from "./config/db"
+import logger from "./middleware/logger"
+
 const app = express()
-const port = 5000
+const port = config.port
 app.use(express.json())
 
-dotenv.config({path: path.join(process.cwd(), ".env")})
-
-const pool = new Pool({
-    connectionString : `${process.env.CONNECTION_STR}`,
-    // ssl: { rejectUnauthorized: false }
-    
-})
 
 
-const initDB = async () =>{
-    await pool.query(`CREATE TABLE IF NOT EXISTS users
-        (
-        id SERIAL PRIMARY  KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(150) UNIQUE NOT NULL,
-        age INT,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-        )`
-    )
-    await pool.query(`CREATE TABLE IF NOT EXISTS todos(
-        id SERIAL PRIMARY  KEY,
-        user_id INT REFERENCES users(id) ON DELETE CASCADE,
-        title VARCHAR(200) NOT NULL,
-        description TEXT,
-        completed BOOLEAN DEFAULT false,
-        due_date DATE,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-        )`)
-}
 
 initDB()
 
-app.get('/', (req:Request, res:Response) => {
+
+
+app.get('/', logger,(req:Request, res:Response) => {
   res.send('Hello next level developers!')
 })
 
@@ -249,6 +223,15 @@ app.put('/todos/:id', async(req:Request, res: Response)=>{
             message : "Data not updated"
         })
     }
+})
+
+
+app.use((req:Request,res:Response)=>{
+    res.status(404).json({
+        success: false,
+        message : "route not found",
+        path: req.path
+    })
 })
 
 app.listen(port, () => {
