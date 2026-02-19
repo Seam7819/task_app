1675b6c66fc86cdb3c0c5bb0040a11776dc0451c
Snu Seam <siamahme766@gmail.com>
2026-02-19 11:39:59 +0600

    login

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import  bcrypt  from 'bcryptjs';
 import { pool } from "../../config/db";
 import jwt from 'jsonwebtoken';
+import config from '../../config';
 
 
 const loginUser = async (email : string , password : string)=>{
@@ -14,8 +15,8 @@ const loginUser = async (email : string , password : string)=>{
     if (!matchedPass){
         return false;
     }
-    const secret = "KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30"
-    const token = await jwt.sign({name : user.name, email: user.email},secret,{
+   
+    const token = await jwt.sign({name : user.name, email: user.email},config.jwt_secret as string,{
         expiresIn : "7d"
     } );
     console.log(token);
